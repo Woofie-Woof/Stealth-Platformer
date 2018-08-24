2e0283a0c8ff38502562b547e8f33de1909212a1
Woofie Woof <ianmisael_99@hotmail.com>
2018-08-23 23:33:48 -0600

    Commit with all project files

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public bool onGround;
+	// Use this for initialization
+	void Start () {
+		onGround = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("left")){
+			transform.Translate(Vector2.left * Time.deltaTime);
+		}
+
+		if(Input.GetKey("right")){
+			transform.Translate(Vector2.right * Time.deltaTime);
+		}
+
+		if(Input.GetKeyDown("space")){
+			if(onGround){
+				transform.Translate(Vector2.up * Time.deltaTime * 10);
+				onGround = false;
+			}
+		}
+	}
+
+	private void OnCollisionEnter(Collision other) {
+		if(other.gameObject.tag == "Ground"){
+			onGround = true;
+		}
+	}
+}
