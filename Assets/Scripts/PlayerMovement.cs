4d86563f49184474eb037bea79ce81f6cfd6c4e8
Woofie Woof <ianmisael_99@hotmail.com>
2018-08-27 00:09:00 -0600

    Added functionality for double jump and some other things related.
    Buggy wall jump in this patch.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public bool onGround;
+	public bool doubleJump;
+	public bool wallJump;
+	public int jumpStrength;
+	private Vector2 wallDir;
+	private Vector2 tempDir;
+	private Rigidbody2D rbd;
 	// Use this for initialization
 	void Start () {
 		onGround = false;
+		doubleJump = false;
+		wallJump = false;
+		rbd = GetComponent<Rigidbody2D>();
+		jumpStrength = 175;
 	}
 	
 	// Update is called once per frame
@@ -21,16 +31,39 @@ public class PlayerMovement : MonoBehaviour {
 		}
 
 		if(Input.GetKeyDown("space")){
-			if(onGround){
-				transform.Translate(Vector2.up * Time.deltaTime * 10);
-				onGround = false;
+			if(onGround || (!wallJump && doubleJump)){
+				rbd.AddForce(Vector2.up * jumpStrength);
+				doubleJump = onGround == false ? false : doubleJump;
+			}
+			else if(wallJump){
+				rbd.AddForce(wallDir * jumpStrength/2);
 			}
 		}
 	}
 
-	private void OnCollisionEnter(Collision other) {
+	private void OnCollisionEnter2D(Collision2D other) {
 		if(other.gameObject.tag == "Ground"){
 			onGround = true;
+			doubleJump = true;
+			wallJump = false;
+		}
+	}
+
+	private void OnCollisionStay2D(Collision2D other) {
+		if(other.gameObject.tag == "Wall" && !onGround){
+			tempDir = other.GetContact(0).point - (new Vector2(transform.position.x, transform.position.y));
+			tempDir = -tempDir.normalized;
+			wallDir = new Vector2(tempDir.x, 1);
+			wallJump = true;
+		}
+	}
+
+	private void OnCollisionExit2D(Collision2D other) {
+		if(other.gameObject.tag == "Ground"){
+			onGround = false;
+		}
+		else if(other.gameObject.tag == "Wall"){
+			wallJump = false;
 		}
 	}
 }
